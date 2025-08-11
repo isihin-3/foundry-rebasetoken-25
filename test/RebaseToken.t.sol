b9a9f3c9288dccb33a037df69154508ae7c5a41a
rizzzhul <rahulkumarmahto334@gmail.com>
2025-08-11 23:04:03 +0530

    rebase token

--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -18,9 +18,11 @@ contract RebaseTokenTest is Test{
         rebaseToken = new RebaseToken();
         vault = new Vault(IRebaseToken(address(rebaseToken)));
         rebaseToken.grantMintAndBurnRole(address(vault));
-        vm.deal(owner,10e18);
-        (bool success,)=payable(address(vault)).call{value: 1e18}("");
         vm.stopPrank();
+    }
+
+    function addRewardsToTheVault(uint256 amount) public{
+        payable(address(vault)).call{value: amount}("");
     }
 
     function testDepositLinear(uint256 amount) public{
@@ -63,14 +65,28 @@ contract RebaseTokenTest is Test{
     }
 
     function testRedeemAfterTimePassed(uint256 amount, uint256 time) public{
-        time = bound(time,1000,type(uint256).max);
+        uint256 maxtime = 60*60*24*365*10;
+        time = bound(time,1000,maxtime);
         uint256 maxAmount = 10000 ether;
         amount = bound(amount,1e5,maxAmount);
 
-        vm.startPrank(user);
+        vm.prank(user);
         vm.deal(user,amount);
         vault.deposit{value: amount}();
+        vm.warp(block.timestamp + time);
 
+        uint256 balance = rebaseToken.balanceOf(user);
 
+        vm.deal(owner,balance - amount);
+        vm.prank(owner);
+        addRewardsToTheVault(balance - amount);
+
+        vm.prank(user);
+        vault.redeem(balance);
+
+        uint256 ethbalance = address(user).balance;
+
+        assertEq(balance , ethbalance);
+        assertGt(balance,amount);
     }
 }
