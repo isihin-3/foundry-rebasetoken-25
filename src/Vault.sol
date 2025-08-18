de66c48d10ff8bb8d82ff7a7e98339f6447ca8cd
rizzzhul <rahulkumarmahto334@gmail.com>
2025-08-18 19:21:15 +0530

    format changes

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,40 +3,39 @@ pragma solidity ^0.8.24;
 
 import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
-contract Vault{
+contract Vault {
     IRebaseToken private immutable i_rebaseToken;
-    
-    event Deposit(address indexed user,uint256 amount);
-    event Redeem(address indexed user,uint256 amount);
+
+    event Deposit(address indexed user, uint256 amount);
+    event Redeem(address indexed user, uint256 amount);
 
     error Vault__TransferFailed();
 
-    constructor( IRebaseToken _rebaseToken){
+    constructor(IRebaseToken _rebaseToken) {
         i_rebaseToken = _rebaseToken;
     }
 
-    receive() external payable{}
+    receive() external payable {}
 
-    function deposit() external payable{
-        i_rebaseToken.mint(msg.sender,msg.value);
-        emit Deposit(msg.sender,msg.value);
+    function deposit() external payable {
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function redeem(uint256 _amount) external{
-         if (_amount == type(uint256).max) {
+    function redeem(uint256 _amount) external {
+        if (_amount == type(uint256).max) {
             _amount = i_rebaseToken.balanceOf(msg.sender);
         }
-        i_rebaseToken.burn(msg.sender,_amount);
-        (bool success,)=payable(msg. sender).call{value: _amount}("");
+        i_rebaseToken.burn(msg.sender, _amount);
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
 
-        if(!success){
+        if (!success) {
             revert Vault__TransferFailed();
         }
-        emit Redeem(msg.sender,_amount);
+        emit Redeem(msg.sender, _amount);
     }
 
-    function getRebaseTokenAddress() external view returns(address){
+    function getRebaseTokenAddress() external view returns (address) {
         return address(i_rebaseToken);
     }
-    
 }
--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Test,console} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {Vault} from "../src/Vault.sol";
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 
-contract RebaseTokenTest is Test{
+contract RebaseTokenTest is Test {
     Vault private vault;
     RebaseToken private rebaseToken;
 
     address public owner = makeAddr("owner");
     address public user = makeAddr("user");
 
-    function setUp() public{
+    function setUp() public {
         vm.startPrank(owner);
         rebaseToken = new RebaseToken();
         vault = new Vault(IRebaseToken(address(rebaseToken)));
@@ -21,63 +21,63 @@ contract RebaseTokenTest is Test{
         vm.stopPrank();
     }
 
-    function addRewardsToTheVault(uint256 amount) public{
+    function addRewardsToTheVault(uint256 amount) public {
         payable(address(vault)).call{value: amount}("");
     }
 
-    function testDepositLinear(uint256 amount) public{
+    function testDepositLinear(uint256 amount) public {
         uint256 maxAmount = 10000 ether;
-        amount = bound(amount,1e5,maxAmount);
+        amount = bound(amount, 1e5, maxAmount);
         vm.startPrank(user);
-        vm.deal(user,amount);
+        vm.deal(user, amount);
         vault.deposit{value: amount}();
 
         uint256 startingBalance = rebaseToken.balanceOf(user);
-        assertEq(amount,startingBalance);
+        assertEq(amount, startingBalance);
 
         vm.warp(block.timestamp + 1 hours);
         uint256 middleBalance = rebaseToken.balanceOf(user);
-        assertGt(middleBalance,startingBalance);
+        assertGt(middleBalance, startingBalance);
 
         vm.warp(block.timestamp + 1 hours);
         uint256 endBalance = rebaseToken.balanceOf(user);
-        assertGt(endBalance,middleBalance);
+        assertGt(endBalance, middleBalance);
 
-        assertApproxEqAbs(endBalance - middleBalance , middleBalance - startingBalance,1);
+        assertApproxEqAbs(endBalance - middleBalance, middleBalance - startingBalance, 1);
 
         vm.stopPrank();
     }
 
-    function testReedemStraightAway(uint256 amount) public{
+    function testReedemStraightAway(uint256 amount) public {
         uint256 maxAmount = 10000 ether;
-        amount = bound(amount,1e5,maxAmount);
+        amount = bound(amount, 1e5, maxAmount);
         vm.startPrank(user);
-        vm.deal(user,amount);
+        vm.deal(user, amount);
 
         vault.deposit{value: amount}();
-        assertEq(rebaseToken.balanceOf(user),amount);
+        assertEq(rebaseToken.balanceOf(user), amount);
 
         vault.redeem(type(uint256).max);
-        assertEq(rebaseToken.balanceOf(user),0);
-        assertEq(address(user).balance,amount);
+        assertEq(rebaseToken.balanceOf(user), 0);
+        assertEq(address(user).balance, amount);
 
         vm.stopPrank();
     }
 
-    function testRedeemAfterTimePassed(uint256 amount, uint256 time) public{
-        uint256 maxtime = 60*60*24*365*10;
-        time = bound(time,1000,maxtime);
+    function testRedeemAfterTimePassed(uint256 amount, uint256 time) public {
+        uint256 maxtime = 60 * 60 * 24 * 365 * 10;
+        time = bound(time, 1000, maxtime);
         uint256 maxAmount = 10000 ether;
-        amount = bound(amount,1e5,maxAmount);
+        amount = bound(amount, 1e5, maxAmount);
 
         vm.prank(user);
-        vm.deal(user,amount);
+        vm.deal(user, amount);
         vault.deposit{value: amount}();
         vm.warp(block.timestamp + time);
 
         uint256 balance = rebaseToken.balanceOf(user);
 
-        vm.deal(owner,balance - amount);
+        vm.deal(owner, balance - amount);
         vm.prank(owner);
         addRewardsToTheVault(balance - amount);
 
@@ -86,7 +86,44 @@ contract RebaseTokenTest is Test{
 
         uint256 ethbalance = address(user).balance;
 
-        assertEq(balance , ethbalance);
-        assertGt(balance,amount);
+        assertEq(balance, ethbalance);
+        assertGt(balance, amount);
+    }
+
+    function testTransfer(uint256 amount, uint256 amountToSend) public {
+        amount = bound(amount, 1e5 + 1e3, type(uint96).max);
+        amountToSend = bound(amountToSend, 1e5, amount - 1e3);
+
+        vm.deal(user, amount);
+        vm.prank(user);
+        vault.deposit{value: amount}();
+
+        address user2 = makeAddr("user2");
+        uint256 userBalance = rebaseToken.balanceOf(user);
+        uint256 user2Balance = rebaseToken.balanceOf(user2);
+        assertEq(userBalance, amount);
+        assertEq(user2Balance, 0);
+
+        vm.prank(owner);
+        rebaseToken.setIntrestRate(4e10);
+
+        vm.prank(user);
+        rebaseToken.transfer(user2, amountToSend);
+        uint256 userBalanceAfterTransfer = rebaseToken.balanceOf(user);
+        uint256 user2BalanceAfterTransfer = rebaseToken.balanceOf(user2);
+        assertEq(user2BalanceAfterTransfer, user2Balance + amountToSend);
+        assertEq(userBalanceAfterTransfer, userBalance - amountToSend);
+
+        vm.warp(block.timestamp + 1 days);
+
+        uint256 userBalanceAfterWarp = rebaseToken.balanceOf(user);
+        uint256 userTwoBalanceAfterWarp = rebaseToken.balanceOf(user2);
+        uint256 userTwoInterestRate = rebaseToken.getUserIntrest(user2);
+        assertEq(userTwoInterestRate, 5e10);
+        uint256 userInterestRate = rebaseToken.getUserIntrest(user);
+        assertEq(userInterestRate, 5e10);
+
+        assertGt(userBalanceAfterWarp, userBalanceAfterTransfer);
+        assertGt(userTwoBalanceAfterWarp, user2BalanceAfterTransfer);
     }
 }
